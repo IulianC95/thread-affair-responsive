82f6a0c9f6650cb488f2d8f287b75be04df772d0
Cuculici Iulian <iuliancuculici@gmail.com>
2023-08-17 21:57:36 +0300

    day 1

---
+++ src/app/page.jsx
@@ -0,0 +1,17 @@
+export default function Home() {
+  return (
+    <>
+      <header className="header">
+        <i className="fa-solid fa-arrow-right"></i>
+      </header>
+
+      <main className="main">
+        <picture>
+          <img src="/images/logo.jpg" alt="logo"></img>
+        </picture>
+      </main>
+
+      <footer className="footer">footer</footer>
+    </>
+  );
+}
